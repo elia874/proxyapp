aae24c8807e3243d92428a717fc25e209863ac16
elia874 <eliacarlos303@gmail.com>
2026-07-31 15:35:20 +0000

    fix: factory reset android folder to default flutter config

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,7 +1,15 @@
 pluginManagement {
-    val r = providers.gradleProperty("flutter.sdk")
-    val flutterSdkPath = if (r.isPresent) r.get() else throw GradleException("flutter.sdk not set")
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
+        }
+
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -11,8 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("com.android.application") version "9.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
 }
 
 include(":app")
